f751df8406f44c154c5b2bf156af99b264974d5b
Chanuka nimsara <chanuka@bistecglobal.com>
2023-12-13 18:00:33 +0530

    upload files

---
+++ lib/ui/theme/color.dart
@@ -0,0 +1,8 @@
+//now let's add the color that we will need to project
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color(0xff1a0800);
+  static Color secondaryColor = Color(0xFF562B00);
+  static Color accentColor = Color(0xffe88141);
+}
